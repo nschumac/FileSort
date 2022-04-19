c927f10f4d868ab9e41ae83dc997b9371b518ae6
joke801 <schum.nikolai@gmail.com>
2022-04-19 14:06:26 +0200

    Add project files.

---
+++ FileSort/FileSort.cpp
@@ -0,0 +1,12 @@
+﻿// FileSort.cpp : Defines the entry point for the application.
+//
+
+#include "FileSort.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
